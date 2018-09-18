6483f63c1a68a370675d140c20b59c911596a0d5
hugo <hugo@illconditioned.com>
2018-09-18 14:40:12 +0000

    libgdf-arrow headers in cffi

--- python/libgdf_cffi/libgdf_build.py
+++ python/libgdf_cffi/libgdf_build.py
@@ -4,8 +4,14 @@ ffibuilder = cffi.FFI()
 ffibuilder.set_source("libgdf_cffi.libgdf_cffi", None)
 
 for fname in ['types.h', 'functions.h']:
+    with open('include-gdf-arrow/gdf-arrow/cffi/{}'.format(fname), 'r') as fin:
+        ffibuilder.cdef(fin.read())
+
+
+for fname in ['types.h', 'functions.h']:
     with open('include/gdf/cffi/{}'.format(fname), 'r') as fin:
         ffibuilder.cdef(fin.read())
+        
 
 if __name__ == "__main__":
     ffibuilder.compile()
